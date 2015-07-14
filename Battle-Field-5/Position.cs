503d7571f7eac806380e312b6d21761fbd58c376
Pariador <pariador.corterian@gmail.com>
2015-07-14 21:31:30 +0300

    Add struct Position.
    Struct Position with X and Y properties.

---
+++ Battle-Field-5/Position.cs
@@ -0,0 +1,54 @@
+﻿namespace BattleField
+{
+    using System;
+
+    public struct Position
+    {
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Position)
+            {
+                return this.Equals((Position)obj);
+            }
+
+            return false;
+        }
+
+        public bool Equals(Position other)
+        {
+
+            return this.X == other.X &&
+                this.Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.X ^ this.Y;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("X = {0}, Y = {1}", this.X, this.Y);
+        }
+
+        public static bool operator ==(Position first, Position second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(Position first, Position second)
+        {
+            return !(first == second);
+        }
+    }
+}
