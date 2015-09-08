3559c1b6040a70d802671e66804f115d9192ac29
Nikolai Novkirishki <novkirishki@yahoo.com>
2015-09-08 15:26:19 +0300

    Added documentation

--- Battle-Field-5/Highscore.cs
+++ Battle-Field-5/Highscore.cs
@@ -1,68 +1,69 @@
-﻿namespace MineFieldApp
+﻿//-----------------------------------------------------------------------
+// <copyright file="Highscore.cs" company="BattleField-5 team">
+//     Telerik teamwork project.
+// </copyright>
+// <summary>
+// Contains Highscore class
+// </summary>
+//-----------------------------------------------------------------------
+namespace MineFieldApp
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Xml.Serialization;
 
+    /// <summary>
+    /// A class using singleton pattern, which gives access to game highscores
+    /// Example usage:
+    ///     Highscore scorer = Highscore.Instance;
+    ///     scorer.AddHighscore("John", 100);
+    ///     foreach (var score in scorer.Highscores)
+    ///     {
+    ///         System.Console.WriteLine(score.PlayerName + " " + score.Points);
+    ///     }
+    /// .
+    /// </summary>
     public sealed class Highscore
     {
-        private string HIGHSCORES_FILE_NAME = "highscores.xml";
+        /// <summary>
+        /// A constant for the name of the file containing the highscores
+        /// </summary>
+        private const string HighscoresFileName = "highscores.xml";
 
-        private static readonly Lazy<Highscore> lazy = new Lazy<Highscore>(() => new Highscore());
+        /// <summary>
+        /// The instance of the Highscore class
+        /// </summary>
+        private static readonly Lazy<Highscore> Lazy = new Lazy<Highscore>(() => new Highscore());
 
+        /// <summary>
+        /// Holds the game highscores
+        /// </summary>
         private List<Score> highscores;
 
-        public static Highscore Instance
-        {
-            get
-            {
-                return lazy.Value;
-            }
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the Highscore class and loads highscores
+        /// </summary>
         private Highscore()
         {
             this.LoadHighscores();
         }
 
-        public void AddHighscore(string playerName, double points)
+        /// <summary>
+        /// Gets the Highscore class instance
+        /// </summary>
+        public static Highscore Instance
         {
-            this.Highscores.Add(new Score(playerName, points));
-            this.Highscores.Sort((x, y) => y.Points.CompareTo(x.Points));
-
-            XmlSerializer writer = new XmlSerializer(typeof(List<Score>));
-
-            StreamWriter file = new StreamWriter(Path.GetFullPath(HIGHSCORES_FILE_NAME), false);
-
-            writer.Serialize(file, this.Highscores);
-
-            file.Close();
-        }
-
-        private void LoadHighscores()
-        {
-            XmlSerializer reader = new XmlSerializer(typeof(List<Score>));
-            StreamReader file = new StreamReader(Path.GetFullPath(HIGHSCORES_FILE_NAME));
-
-            try
+            get
             {
-                this.Highscores = (List<Score>)reader.Deserialize(file);
+                return Lazy.Value;
             }
-            catch (InvalidOperationException)
-            {
-
-            }
-
-            file.Close();
         }
 
-        public void ClearHighscores()
-        {
-            this.Highscores.Clear();
-            File.WriteAllText(Path.GetFullPath(HIGHSCORES_FILE_NAME), String.Empty);
-        }
-
+        /// <summary>
+        /// Gets or sets the highscores. It implements lazy loading pattern: 
+        /// if the list with highscores is null it is created.
+        /// </summary>
         public List<Score> Highscores
         {
             get
@@ -79,6 +80,53 @@
             {
                 this.highscores = value;
             }
+        }
+
+        /// <summary>
+        /// Adds a highscore to the file and a the list with highscores
+        /// </summary>
+        /// <param name="playerName">Name of the player setting high score</param>
+        /// <param name="points">His result</param>
+        public void AddHighscore(string playerName, double points)
+        {
+            this.Highscores.Add(new Score(playerName, points));
+            this.Highscores.Sort((x, y) => y.Points.CompareTo(x.Points));
+
+            XmlSerializer writer = new XmlSerializer(typeof(List<Score>));
+
+            StreamWriter file = new StreamWriter(Path.GetFullPath(HighscoresFileName), false);
+
+            writer.Serialize(file, this.Highscores);
+
+            file.Close();
+        }
+
+        /// <summary>
+        /// Clears all highscores
+        /// </summary>
+        public void ClearHighscores()
+        {
+            this.Highscores.Clear();
+            File.WriteAllText(Path.GetFullPath(HighscoresFileName), string.Empty);
+        }
+
+        /// <summary>
+        /// Reads highscores from the file and writes them to the list with highscores
+        /// </summary>
+        private void LoadHighscores()
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(List<Score>));
+            StreamReader file = new StreamReader(Path.GetFullPath(HighscoresFileName));
+
+            try
+            {
+                this.Highscores = (List<Score>)reader.Deserialize(file);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+
+            file.Close();
         }
     }
 }
--- Battle-Field-5/Score.cs
+++ Battle-Field-5/Score.cs
@@ -1,13 +1,58 @@
-﻿namespace MineFieldApp
+﻿//-----------------------------------------------------------------------
+// <copyright file="Score.cs" company="BattleField-5 team">
+//     Telerik teamwork project.
+// </copyright>
+// <summary>
+// Contains Score class
+// </summary>
+//-----------------------------------------------------------------------
+namespace MineFieldApp
 {
     using System;
 
+    /// <summary>
+    /// Class representing a players score
+    /// </summary>
     public class Score
     {
+        /// <summary>
+        /// Holds the value of players name
+        /// </summary>
         private string playerName;
+
+        /// <summary>
+        /// Holds the value of players points 
+        /// </summary>
         private double points;
+
+        /// <summary>
+        /// Holds the date of creation of the score
+        /// </summary>
         private DateTime date;
 
+        /// <summary>
+        /// Initializes a new instance of the Score class
+        /// </summary>
+        /// <param name="playerName">The name of the player</param>
+        /// <param name="points">The points of the player</param>
+        public Score(string playerName, double points)
+        {
+            this.PlayerName = playerName;
+            this.Points = points;
+            this.Date = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Score class. This emplty constructor
+        /// is obligatory because of the serialization.
+        /// </summary>
+        public Score()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets players name
+        /// </summary>
         public string PlayerName
         {
             get
@@ -21,6 +66,9 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets points
+        /// </summary>
         public double Points
         {
             get
@@ -34,6 +82,9 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets scores data
+        /// </summary>
         public DateTime Date
         {
             get
@@ -45,18 +96,6 @@
             {
                 this.date = value;
             }
-        }
-
-        public Score(string playerName, double points)
-        {
-            this.PlayerName = playerName;
-            this.Points = points;
-            this.Date = DateTime.Now;
-        }
-
-        public Score()
-        {
-
         }
     }
 }
