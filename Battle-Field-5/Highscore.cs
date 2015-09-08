61b10e9e154785e154320a54c8ae0b5670c354fc
Nikolai Novkirishki <novkirishki@yahoo.com>
2015-09-08 14:58:45 +0300

    Added Score class and Highscore logic
    Implemented singleton and lazy loading patterns

---
+++ Battle-Field-5/Highscore.cs
@@ -0,0 +1,84 @@
+﻿namespace MineFieldApp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    public sealed class Highscore
+    {
+        private string HIGHSCORES_FILE_NAME = "highscores.xml";
+
+        private static readonly Lazy<Highscore> lazy = new Lazy<Highscore>(() => new Highscore());
+
+        private List<Score> highscores;
+
+        public static Highscore Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        private Highscore()
+        {
+            this.LoadHighscores();
+        }
+
+        public void AddHighscore(string playerName, double points)
+        {
+            this.Highscores.Add(new Score(playerName, points));
+            this.Highscores.Sort((x, y) => y.Points.CompareTo(x.Points));
+
+            XmlSerializer writer = new XmlSerializer(typeof(List<Score>));
+
+            StreamWriter file = new StreamWriter(Path.GetFullPath(HIGHSCORES_FILE_NAME), false);
+
+            writer.Serialize(file, this.Highscores);
+
+            file.Close();
+        }
+
+        private void LoadHighscores()
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(List<Score>));
+            StreamReader file = new StreamReader(Path.GetFullPath(HIGHSCORES_FILE_NAME));
+
+            try
+            {
+                this.Highscores = (List<Score>)reader.Deserialize(file);
+            }
+            catch (InvalidOperationException)
+            {
+
+            }
+
+            file.Close();
+        }
+
+        public void ClearHighscores()
+        {
+            this.Highscores.Clear();
+            File.WriteAllText(Path.GetFullPath(HIGHSCORES_FILE_NAME), String.Empty);
+        }
+
+        public List<Score> Highscores
+        {
+            get
+            {
+                if (this.highscores == null)
+                {
+                    this.highscores = new List<Score>();
+                }
+
+                return this.highscores;
+            }
+
+            private set
+            {
+                this.highscores = value;
+            }
+        }
+    }
+}
---
+++ Battle-Field-5/Score.cs
@@ -0,0 +1,62 @@
+﻿namespace MineFieldApp
+{
+    using System;
+
+    public class Score
+    {
+        private string playerName;
+        private double points;
+        private DateTime date;
+
+        public string PlayerName
+        {
+            get
+            {
+                return this.playerName;
+            }
+
+            set
+            {
+                this.playerName = value;
+            }
+        }
+
+        public double Points
+        {
+            get
+            {
+                return this.points;
+            }
+
+            set
+            {
+                this.points = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return this.date;
+            }
+
+            set
+            {
+                this.date = value;
+            }
+        }
+
+        public Score(string playerName, double points)
+        {
+            this.PlayerName = playerName;
+            this.Points = points;
+            this.Date = DateTime.Now;
+        }
+
+        public Score()
+        {
+
+        }
+    }
+}
