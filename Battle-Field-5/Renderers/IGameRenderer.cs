ed2aecdebda5854680b068a0ad1a7223af80ad4d
Ivorankov <ivo_rankov@mail.bg>
2015-09-18 01:58:53 +0300

    Initial structure commit

---
+++ Battle-Field-5/Renderers/IGameRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattleField.Renderers
+{
+    interface IGameRenderer
+    {
+        void ShowStartScreen();
+
+        void ShowEndScreen();
+
+        void DrawGameField();
+
+        void Clear();
+
+    }
+}
